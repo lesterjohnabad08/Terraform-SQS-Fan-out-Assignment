c8c1b2846866ee33da0551878ab31b39dd6dbbb9
lesterjohnabad08 <lesterjohnabad@gmail.com>
2026-05-05 01:38:44 -0500

    Add IAM role and policy for Lambda function and update README with Lambda documentation link

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/sns_topic
 https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/sns_topic_subscription
+https://registry.terraform.io/providers/-/aws/latest/docs/resources/lambda_function
+https://registry.terraform.io/providers/-/aws/latest/docs/resources/iam_role
+
--- main.tf
+++ main.tf
@@ -23,3 +23,45 @@ resource "aws_sns_topic_subscription" "New-S3-Obj-Uploaded-Event-Msg-Subscriptio
   protocol             = "email"
   endpoint             = var.email_address
 }
+
+resource "aws_s3_bucket" "sqs-fan-out-bucket-ljabad" {
+  bucket = "sqs-fan-out-bucket-ljabad"
+
+}
+
+resource "aws_iam_role" "lambda_s3_allow_role" {
+  name = "lambda-s3-allow-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+
+  tags = {
+    Name = "Lambda-S3-Allow-Role"
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "s3_full_access" {
+  role      = aws_iam_role.lambda_s3_allow_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
+
+#Editing this
+
+#resource "aws_lambda_function" "example" {
+ # filename      = "${path.module}/lambda.zip"
+ # function_name = "example_lambda_function"
+ # role          = aws_iam_role.example.arn 
+ # handler       = "lambda_function.handler"
+ # runtime       = "python3.12"
+
+#}
