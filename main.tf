fa6f89fc94c406ab3ddbd212b6d2e5342210c6a3
lesterjohnabad08 <lesterjohnabad@gmail.com>
2026-05-05 21:11:32 -0500

    Add IAM policy for SNS topic, update resource names, and configure S3 bucket notifications

--- main.tf
+++ main.tf
@@ -13,24 +13,74 @@ provider "aws" {
   region  = "us-east-1"
 }
 
-#Create SNS Topic
-resource "aws_sns_topic" "New-S3-Obj-Uploaded-Event-Msg" {
-  name = "New-S3-Obj-Uploaded-Event-Msg"
+data "aws_iam_policy_document" "topic" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["s3.amazonaws.com"]
+    }
+
+    actions   = ["SNS:Publish"]
+    resources = ["arn:aws:sns:*:*:New_S3_Obj_Uploaded_Event_Msg"]
+
+    condition {
+      test     = "ArnLike"
+      variable = "aws:SourceArn"
+      values   = [aws_s3_bucket.sqs_fan_out_bucket_ljabad.arn]
+    }
+  }
 }
 
-resource "aws_sns_topic_subscription" "New-S3-Obj-Uploaded-Event-Msg-Subscription" {
-  topic_arn            = aws_sns_topic.New-S3-Obj-Uploaded-Event-Msg.arn
+#Create SNS Topic
+resource "aws_sns_topic" "New_S3_Obj_Uploaded_Event_Msg" {
+  name = "New_S3_Obj_Uploaded_Event_Msg"
+  policy = data.aws_iam_policy_document.topic.json
+}
+
+#create SQS_Queue
+resource "aws_sqs_queue" "sqs_fanout_new_s3_obj_event" {
+  name                      = "sqs_fanout_new_s3_obj_event"
+  delay_seconds             = 0
+  max_message_size          = 2048
+  message_retention_seconds = 86400
+  receive_wait_time_seconds = 0  
+}
+
+resource "aws_sns_topic_subscription" "New_S3_Obj_Uploaded_Event_Msg_Subscription" {
+  topic_arn            = aws_sns_topic.New_S3_Obj_Uploaded_Event_Msg.arn
   protocol             = "email"
   endpoint             = var.email_address
 }
 
-resource "aws_s3_bucket" "sqs-fan-out-bucket-ljabad" {
-  bucket = "sqs-fan-out-bucket-ljabad"
-
+resource "aws_sns_topic_subscription" "SQS_Queue_Event_Msg_Subscription" {
+  topic_arn            = aws_sns_topic.New_S3_Obj_Uploaded_Event_Msg.arn
+  protocol             = "sqs"
+  endpoint             = aws_sqs_queue.sqs_fanout_new_s3_obj_event.arn
 }
 
-resource "aws_iam_role" "lambda_s3_allow_role" {
-  name = "lambda-s3-allow-role"
+resource "aws_s3_bucket" "sqs_fan_out_bucket_ljabad" {
+  bucket = "sqs-fan-out-bucket-ljabad"
+}
+
+resource "aws_s3_bucket" "sqs_fan_out_bucket_ljabad_resized" {
+  bucket = "sqs-fan-out-bucket-ljabad-resized"
+}
+
+resource "aws_s3_bucket_notification" "sqs_fan_out_new_s3_obj_msg_ljabad" {
+  bucket = aws_s3_bucket.sqs_fan_out_bucket_ljabad.id
+
+  topic {
+    topic_arn     = aws_sns_topic.New_S3_Obj_Uploaded_Event_Msg.arn
+    events        = ["s3:ObjectCreated:*"]
+    
+  }
+}
+
+#create Lambda_SQS_Role ********
+resource "aws_iam_role" "lambda_s3_sqs_allow_role" {
+  name = "lambda_s3_sqs_allow_role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -46,13 +96,18 @@ resource "aws_iam_role" "lambda_s3_allow_role" {
   })
 
   tags = {
-    Name = "Lambda-S3-Allow-Role"
+    Name = "Lambda_S3_SQS_Allow_Role"
   }
 }
 
 resource "aws_iam_role_policy_attachment" "s3_full_access" {
-  role      = aws_iam_role.lambda_s3_allow_role.name
+  role      = aws_iam_role.lambda_s3_sqs_allow_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "sqs_full_access" {
+  role      = aws_iam_role.lambda_s3_sqs_allow_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSQSFullAccess"
 }
 
 #Editing this
